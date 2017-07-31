5d72b885cb1fb5dfca4713b3ca5dde73aef5bce6
Matt <mguest55@yahoo.com>
2017-07-31 11:41:17 -0700

    First Commit

---
+++ PuzzleSlider/GameActivity.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PuzzleSlider
+{
+    [Activity(Label = "GameActivity")]
+    public class GameActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Game);
+			//Make sure it is android resource and use .axml
+			//https://www.youtube.com/watch?v=EAr098PHt3w
+            //good tutorials
+
+			// Create your application here
+		
+        }
+    }
+}
---
+++ PuzzleSlider/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Lang;
+using System;
+using Android.Content;
+
+namespace PuzzleSlider
+{
+    [Activity(Label = "Puzzle Slider", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+		Button mEasyButton;
+		Button mMediumButton;
+		Button mHardButton;
+		Button mImpossibleButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it - We have 4 buttons
+            Button mEasyButton = FindViewById<Button>(Resource.Id.Easy_Button);
+            mEasyButton.Click += mEasyButton_Click;
+
+            Button mMediumButton = FindViewById<Button>(Resource.Id.Medium_Button);
+			mMediumButton.Click += mMediumButton_Click;
+
+            Button mHardButton = FindViewById<Button>(Resource.Id.Hard_Button);
+			mHardButton.Click += mHardButton_Click;
+
+            Button mImpossibleButton = FindViewById<Button>(Resource.Id.Impossible_Button);
+			mImpossibleButton.Click += mImpossibleButton_Click;
+        }
+
+        //Each button leads to same game view but modifies game board slightly
+        void mEasyButton_Click(object sender, EventArgs e) 
+        {
+            Intent intent = new Intent();
+        }
+
+		void mMediumButton_Click(object sender, EventArgs e)
+		{
+			Intent intent = new Intent();
+		}
+
+		void mHardButton_Click(object sender, EventArgs e)
+		{
+			Intent intent = new Intent();
+		}
+
+		void mImpossibleButton_Click(object sender, EventArgs e)
+		{
+			Intent intent = new Intent();
+		}
+
+        public static int DifficultyLevel(Button difficulty) 
+        {
+			//switch statement for which ever button was tapped
+            switch (difficulty.Id)
+			{
+                case Resource.Id.Easy_Button:
+					//Do stuff for item1
+                    return 1;
+                case Resource.Id.Medium_Button:
+					//Do stuff for item2
+					return 2;
+                case Resource.Id.Hard_Button:
+					//Do stuff for item3
+					return 3;
+                case Resource.Id.Impossible_Button:
+					//Do stuff for item3
+					return 4;
+				default:
+					return 0;
+			}
+        }
+    }
+}
+
